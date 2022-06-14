763f14845f30a0a5ac85b6ed1e11706b02a46bc8
Ross Nesbit <rnesbit@nexus-network.cloud>
2022-06-14 14:02:56 -0500

    Initial Code Push

---
+++ src/CsharpOpenWeatherMap/OpenWeatherMapClient.cs
@@ -0,0 +1,41 @@
+﻿using CsharpOpenWeatherMap;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusNetworkCloud.CsharpOpenWeatherMap
+{
+    public class OpenWeatherMapClient
+    {
+        private static string address = "https://api.openweathermap.org/data/2.5";
+        private readonly HttpClient client = new HttpClient();
+
+        private readonly string _appId;
+
+        public OpenWeatherMapClient(string appId)
+        {
+            _appId = appId;
+        }
+
+        ~OpenWeatherMapClient()
+        {
+            client.Dispose();
+        }
+
+        public async Task<WeatherReportResponse> GetCurrentWeatherByZipCodeAsync(int zip)
+        {
+            if (Math.Floor(Math.Log10(zip) + 1) != 5)
+                throw new ArgumentException("Invalid ZIP Format Detected, Please Ensure That The Entered ZIP Code Is 5 Digits Long");
+
+            HttpResponseMessage res = await client.GetAsync($"{address}/weather?zip={zip}&units=imperial&appid={_appId}");
+
+            if (!res.IsSuccessStatusCode)
+                return new WeatherReportResponse { RequestSuccessful = false, RequestError = await res.Content.ReadAsStringAsync() };
+            else
+                return new WeatherReportResponse { RequestSuccessful = true, WeatherReport = JObject.Parse(await res.Content.ReadAsStringAsync()).ToObject<WeatherReport>() };
+        }
+    }
+}
---
+++ src/CsharpOpenWeatherMap/WeatherReport.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CsharpOpenWeatherMap
+{
+    public class WeatherReport
+    {
+        [JsonProperty("coord")]
+        public OWMCoordinate Coordinates { get; set; } = new();
+
+        [JsonProperty("weather")]
+        public List<OWMWeather> Weather { get; set; } = new();
+
+        [JsonProperty("base")]
+        public string? Base { get; set; }
+
+        [JsonProperty("main")]
+        public OWMMain Main { get; set; } = new();
+
+        [JsonProperty("visibility")]
+        public int Visibility { get; set; }
+
+        [JsonProperty("wind")]
+        public OWMWind Wind { get; set; } = new();
+
+        [JsonProperty("clouds")]
+        public OWMClouds Clouds { get; set; } = new();
+
+        [JsonProperty("dt")]
+        public long DateTimeUnixUTC { get; set; }
+
+        [JsonIgnore]
+        public DateTime LastUpdated { get { return new DateTime(1970, 1, 1).AddSeconds(DateTimeUnixUTC).ToLocalTime(); } }
+
+        [JsonProperty("sys")]
+        public OWMSystem System { get; set; } = new();
+
+        [JsonProperty("timezone")]
+        public int Timezone { get; set; }
+
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("cod")]
+        public int Code { get; set; }
+
+        // Custom Variables
+        public bool IsDaytime { get { return DateTime.Compare(DateTime.Now, System.Sunset) < 0 && DateTime.Compare(DateTime.Now, System.Sunrise) > 0; } }
+        public bool IsLight { get { return IsDaytime && Clouds?.Cloudiness < 95; } }
+    }
+
+    public class OWMCoordinate
+    {
+        [JsonProperty("lon")]
+        public float Longitude { get; set; }
+
+        [JsonProperty("lat")]
+        public float Latitude { get; set; }
+    }
+
+    public class OWMWeather
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("main")]
+        public string? Main { get; set; }
+
+        [JsonProperty("description")]
+        public string? Description { get; set; }
+
+        [JsonProperty("icon")]
+        public string? Icon { get; set; }
+    }
+
+    public class OWMMain
+    {
+        [JsonProperty("temp")]
+        public float Temperature { get; set; }
+
+        [JsonProperty("feels_like")]
+        public float FeelsLike { get; set; }
+
+        [JsonProperty("temp_min")]
+        public float TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public float TempMax { get; set; }
+
+        [JsonProperty("pressure")]
+        public int Pressure { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+    }
+
+    public class OWMWind
+    {
+        [JsonProperty("speed")]
+        public float Speed { get; set; }
+
+        [JsonProperty("deg")]
+        public int Degree { get; set; }
+
+        [JsonProperty("gust")]
+        public float Gust { get; set; }
+    }
+
+    public class OWMClouds
+    {
+        [JsonProperty("all")]
+        public int Cloudiness { get; set; }
+    }
+
+    [JsonConverter(typeof(UnixDateTimeConverter))]
+    public class OWMSystem
+    {
+        [JsonProperty("country")]
+        public string? CountryCode { get; set; }
+
+        [JsonProperty("sunrise")]
+        public DateTime Sunrise { get; set; }
+
+        [JsonProperty("sunset")]
+        public DateTime Sunset { get; set; }
+    }
+
+    public class UnixDateTimeConverter : JsonConverter<OWMSystem>
+    {
+        public override bool CanWrite => false;
+
+        public override OWMSystem? ReadJson(JsonReader reader, Type objectType, OWMSystem? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            OWMSystem system = new();
+
+            JObject jObject = JObject.Load(reader);
+
+            system.CountryCode = jObject.Value<string>("country");
+            system.Sunrise = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(jObject.Value<int>("sunrise")).ToLocalTime();
+            system.Sunset = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(jObject.Value<int>("sunset")).ToLocalTime();
+
+            return system;
+        }
+
+        public override void WriteJson(JsonWriter writer, OWMSystem? value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WeatherReportResponse
+    {
+        public bool RequestSuccessful { get; set; }
+        public string? RequestError { get; set; }
+        public WeatherReport? WeatherReport { get; set; }
+    }
+}
